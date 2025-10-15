b2235144e71c7d28318e066907ab7d429be6a7b8
Tursunov Muhammadsayyid <114228094+SayyeedDeveloper@users.noreply.github.com>
2025-10-15 17:57:14 +0500

    Home screen (#4)
    * AuthScreen & Navigation setup
    
    * Home Screen

---
+++ app/src/main/java/com/example/githublab/HomeScreen.kt
@@ -0,0 +1,120 @@
+package com.example.githublab
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Home"
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    onClick = {navController.navigate(Screens.Auth.route)},
+                    selected = true,
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.logout_24px),
+                            contentDescription = "Home")
+                    },
+                    label = {
+                        Text(
+                            "Log Out"
+                        )
+                    }
+                )
+                NavigationBarItem(
+                    onClick = {navController.navigate(Screens.Profile.route)},
+                    selected = true,
+                    icon = {
+                        Icon(
+                            Icons.Default.Person,
+                            contentDescription = "Home")
+                    },
+                    label = {
+                        Text(
+                            "Profile"
+                        )
+                    }
+                )
+            }
+        }
+    ){ innerPadding ->
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentPadding = PaddingValues(14.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ){
+            items(10) { index ->
+                Text(
+                    text = "Index ${index}",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = MaterialTheme.colorScheme.primaryContainer,
+                            shape = RoundedCornerShape(14.dp)
+                        )
+                        .shadow(
+                            elevation = 1.dp,
+                            shape = RoundedCornerShape(14.dp)
+                        )
+                        .padding(16.dp),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPrev(){
+    HomeScreen(
+        navController = androidx.navigation.compose.rememberNavController(),
+        modifier = Modifier
+    )
+}
